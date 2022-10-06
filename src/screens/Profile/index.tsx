865d4986bc775d90acfa0a52e281385a4805cff7
Luiz Teixeira <luizhbtluiz@gmail.com>
2022-10-06 09:35:52 -0300

    feat: logout

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TouchableOpacity, StyleSheet } from "react-native";
 import { useAuth } from "../../contexts/auth";
+import { MaterialIcons } from "@expo/vector-icons";
 
 // import { Container } from './styles';
 
@@ -8,10 +9,29 @@ const Profile: React.FC = () => {
   const { signOut } = useAuth();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile Screen</Text>
-      <Button title="sair" onPress={signOut}></Button>
+      <TouchableOpacity
+        style={{ ...styles.button, backgroundColor: "orange" }}
+        onPress={signOut}
+      >
+        <Text style={{ marginRight: 10 }}>Deslogar</Text>
+        <MaterialIcons name="logout" size={24} color="black" />
+      </TouchableOpacity>
+      {/* <Text>Profile Screen</Text> */}
+      {/* <Button title="sair" onPress={signOut}></Button> */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "center",
+    padding: 12,
+    borderRadius: 5,
+    width: "80%",
+  },
+});
 
 export default Profile;
