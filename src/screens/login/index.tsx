8c0735629f430045029f8295a8e8f3e667b5fb26
Luiz Teixeira <luizhbtluiz@gmail.com>
2022-10-06 08:42:54 -0300

    feat: style login

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,6 +1,13 @@
 import React, { useContext } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { useAuth } from "../../contexts/auth";
 
 type FormData = {
@@ -27,61 +34,81 @@ const Login = ({ navigation }: any): JSX.Element => {
     await signIn(data);
   };
 
-  console.log("errors", errors);
-
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Text style={styles.label}>First name</Text>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            autoCapitalize="none"
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-          />
-        )}
-        name="email"
-        rules={{ required: true }}
-      />
-      <Text style={styles.label}>Last name</Text>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-          />
-        )}
-        name="password"
-        rules={{ required: true }}
-      />
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          width: "90%",
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <Text
+          style={{
+            marginVertical: 40,
+            alignSelf: "center",
+            fontSize: 68,
+            color: "purple",
+          }}
+        >
+          KeepCar
+        </Text>
+        <Text style={styles.label}>Email</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              autoCapitalize="none"
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="email"
+          rules={{ required: true }}
+        />
+        <Text style={styles.label}>Senha</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+              secureTextEntry
+            />
+          )}
+          name="password"
+          rules={{ required: true }}
+        />
 
-      <View style={styles.button}>
-        <Button
-          title={false ? "Carregando..." : "Login"}
+        <TouchableOpacity
+          style={styles.button2}
           onPress={handleSubmit(onSubmit)}
           disabled={loading}
+        >
+          {/* <MaterialIcons name="add-circle" size={24} color="white" /> */}
+          <Text style={{ marginLeft: 10, color: "white", fontSize: 18 }}>
+            {loading ? "Carregando..." : "Login"}
+          </Text>
+        </TouchableOpacity>
+
+        <Button
+          title="Go to Register"
+          onPress={() => navigation.navigate("Register")}
         />
       </View>
-      {/* <View>{JSON.stringify(user) ?? "teste"}</View> */}
-
-      <Button
-        title="Go to Register"
-        onPress={() => navigation.navigate("Register")}
-      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   label: {
-    margin: 20,
+    marginTop: 20,
+    marginBottom: 5,
     marginLeft: 0,
   },
   button: {
@@ -90,14 +117,26 @@ const styles = StyleSheet.create({
     height: 40,
     backgroundColor: "#ec5990",
     borderRadius: 4,
-    width: "80%",
+    //width: "80%",
   },
   input: {
     backgroundColor: "white",
-    height: 40,
+    height: 50,
     padding: 10,
     borderRadius: 4,
-    width: "80%",
+    //width: "80%",
+  },
+  button2: {
+    flexDirection: "row",
+    alignItems: "center",
+    // backgroundColor: "white",
+    backgroundColor: "purple",
+    justifyContent: "center",
+    padding: 10,
+    borderRadius: 5,
+    height: 50,
+    marginTop: 40,
+    marginBottom: 5,
   },
 });
 
